cc484a222604027f2d19b024851d27b457bd2e1d
soymusta <mustaelhachmimahti@gmail.com>
2022-11-17 22:15:16 +0100

    wp

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
 import GitCi from "./classes/git-ci";
+import { schedule } from "node-cron";
+import * as child from "child_process";
+import * as util from "util";
+import * as fs from "fs";
+import { COMMAND_GET_LAST_COMMIT, CRON } from "./constants";
 
 function main() {
   new GitCi();
 }
 
-main();
+// main();
+
+const _exec = util.promisify(child.exec);
+
+const test = async () => {
+  const { stdout } = await _exec(
+    'git for-each-ref refs/remotes/origin --sort="-committerdate" | head -1\n'
+  );
+  console.log(stdout.split(" ")[0]);
+};
+test();
--- src/script/last-commit.sh
+++ src/script/last-commit.sh
@@ -1,4 +1,1 @@
-git checkout dev
-git pull origin dev
-git checkout feature/ci-work-v2
 git rev-parse dev
