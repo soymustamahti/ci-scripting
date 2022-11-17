7c28df4e0138bc26cec8fea3f8a11c0a395837c3
soymusta <mustaelhachmimahti@gmail.com>
2022-11-17 16:41:32 +0100

    wp crono check every minut commit and store it

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log("Hello");
+import {schedule} from 'node-cron'
+import * as child from 'child_process';
+import * as util from 'util';
+import * as fs from 'fs'
+
+
+const _exec = util.promisify(child.exec);
+
+schedule('*/5 * * * * *',async  () => {
+try{
+  const { stdout } = await _exec(
+    `bash src/script/script.sh`
+  );
+  await tmp(stdout)
+}catch(err){
+  console.log(err);
+  
+}
+});
+
+async function tmp(stdout: string){
+try{
+
+ const tmp = await fs.writeFileSync('src/store/commit.txt', stdout)
+ console.log(tmp);
+}catch(err){
+  console.log(err)
+}
+}
