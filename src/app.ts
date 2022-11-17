21e9b3a87936b19a220e7d75188f93d3a363b25d
soymusta <mustaelhachmimahti@gmail.com>
2022-11-17 20:26:04 +0100

    Changer strocture to class and clean some code

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,7 @@
-import {schedule} from 'node-cron'
-import * as child from 'child_process';
-import * as util from 'util';
-import * as fs from 'fs'
+import GitCi from "./classes/git-ci";
 
-
-const _exec = util.promisify(child.exec);
-
-schedule('*/5 * * * * *',async  () => {
-try{
-  const { stdout } = await _exec(
-    `bash src/script/script.sh`
-  );
-  await tmp(stdout)
-}catch(err){
-  console.log(err);
-  
+function main() {
+  new GitCi();
 }
-});
 
-async function tmp(stdout: string){
-try{
-
- const tmp = await fs.writeFileSync('src/store/commit.txt', stdout)
- console.log(tmp);
-}catch(err){
-  console.log(err)
-}
-}
+main();
