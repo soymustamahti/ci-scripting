73dfc53e852786669980fdea6827c7384718ffcb
soymusta <mustaelhachmimahti@gmail.com>
2022-11-21 21:26:51 +0100

    Fix const

--- src/constants/git.ts
+++ src/constants/git.ts
@@ -1,1 +1,3 @@
-const GET_CURRENT_BRANCH_NAME = 'git rev-parse --abbrev-ref HEAD';
+export const COMMAND_GET_LAST_COMMIT = `bash src/script/last-commit.sh`;
+export const COMMAND_REBASE_FAST_FORWARD = `bash src/script/rebase.sh`;
+export const COMMAND_CURRENT_BRANCH_NAME = 'git rev-parse --abbrev-ref HEAD';
