f078f47b8e9a1e3efa93ffd52cf07b029d38e55c
soymusta <mustaelhachmimahti@gmail.com>
2022-11-22 00:47:36 +0100

    Upadate function convertToRoman for typing issues

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,20 +1,20 @@
 export class Calculator {
   value: number;
-  lookup: any = {
-    M: 1000,
-    CM: 900,
-    D: 500,
-    CD: 400,
-    C: 100,
-    XC: 90,
-    L: 50,
-    XL: 40,
-    X: 10,
-    IX: 9,
-    V: 5,
-    IV: 4,
-    I: 1,
-  };
+  lookup = [
+   { M: 1000},
+    {CM: 900},
+    {D: 500},
+    {CD: 400},
+    {C: 100},
+    {XC: 90},
+    {L: 50},
+    {XL: 40},
+    {X: 10},
+    {IX: 9},
+    {V: 5},
+    {IV: 4},
+    {I: 1},
+  ]
 
   constructor() {
     this.value = 0;
@@ -26,16 +26,15 @@ export class Calculator {
   }
 
   convertToRoman(num: number) {
-    let roman = "";
-    let i;
+    let roman = '';
     if (num < 1 || num > 99999)
-      return `Not posible number too ${
-        num < 1 ? "small" : "big"
-      }, please enter a number between 1 and 99999`;
-    for (i in this.lookup) {
-      while (num >= this.lookup[i]) {
-        roman += i;
-        num -= this.lookup[i];
+    return `Not posible number too ${
+      num < 1 ? "small" : "big"
+    }, please enter a number between 1 and 99999`;
+    for (let i = 0; i < this.lookup.length; i++) {
+      while (num >= Object.values(this.lookup[i])[0]) {
+        roman += Object.keys(this.lookup[i])[0];
+        num -= Object.values(this.lookup[i])[0];
       }
     }
     return roman;
