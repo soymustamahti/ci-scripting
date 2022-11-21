9e9280ecc7b2461b0a081dd35251e30255ee4bc1
soymusta <mustaelhachmimahti@gmail.com>
2022-11-22 00:48:58 +0100

    Fix codestyle

--- src/calculator.ts
+++ src/calculator.ts
@@ -1,20 +1,20 @@
 export class Calculator {
   value: number;
   lookup = [
-   { M: 1000},
-    {CM: 900},
-    {D: 500},
-    {CD: 400},
-    {C: 100},
-    {XC: 90},
-    {L: 50},
-    {XL: 40},
-    {X: 10},
-    {IX: 9},
-    {V: 5},
-    {IV: 4},
-    {I: 1},
-  ]
+    { M: 1000 },
+    { CM: 900 },
+    { D: 500 },
+    { CD: 400 },
+    { C: 100 },
+    { XC: 90 },
+    { L: 50 },
+    { XL: 40 },
+    { X: 10 },
+    { IX: 9 },
+    { V: 5 },
+    { IV: 4 },
+    { I: 1 },
+  ];
 
   constructor() {
     this.value = 0;
@@ -26,11 +26,11 @@ export class Calculator {
   }
 
   convertToRoman(num: number) {
-    let roman = '';
+    let roman = "";
     if (num < 1 || num > 99999)
-    return `Not posible number too ${
-      num < 1 ? "small" : "big"
-    }, please enter a number between 1 and 99999`;
+      return `Not posible number too ${
+        num < 1 ? "small" : "big"
+      }, please enter a number between 1 and 99999`;
     for (let i = 0; i < this.lookup.length; i++) {
       while (num >= Object.values(this.lookup[i])[0]) {
         roman += Object.keys(this.lookup[i])[0];
