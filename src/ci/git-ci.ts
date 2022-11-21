59000dbf34b8ed1e8e1b09c1f9c22a2b0d2a3f65
soymusta <mustaelhachmimahti@gmail.com>
2022-11-21 21:32:51 +0100

    fix promis issue about current branch name

--- src/ci/git-ci.ts
+++ src/ci/git-ci.ts
@@ -40,7 +40,7 @@ export default class GitCi {
         const result = await this.runTest();
         console.log("RESULT TEST", result);
         if (result) {
-          const currentBranchName = this.getCurrentBranchName()
+          const currentBranchName = await this.getCurrentBranchName()
           await this.sleep(1000);
             const { stdout } = await this._exec(COMMAND_REBASE_FAST_FORWARD + currentBranchName);
             console.log("REBASE", stdout);
