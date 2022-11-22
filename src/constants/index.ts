c6caaeeea74d09db07a800c5be6c461875f04780
soymusta <mustapha@jump-biz.com>
2022-11-22 11:47:31 +0100

    change crono run each 20 s

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
 export const FILE_PATH = "src/store/commit.txt";
 export const ENCODING = "utf8";
 export const COMMAND_TEST = `npm run test`;
-export const CRON = "*/10 * * * * *";
+export const CRON = "*/20 * * * * *";
